64d9ff8b86e075dc91cd835b2fdc87d97da0628f
Lobão <andreycode@hotmail.com>
2023-04-24 20:28:59 -0300

    Adicionar arquivos de projeto.

---
+++ lojaAdm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace lojaAdm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
